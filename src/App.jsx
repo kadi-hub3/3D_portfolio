d8adcfc4e1e662f49fb16711ee4dafa57d0e9775
Kadi M <kicktheball1@gmail.com>
2024-07-06 19:05:41 +0200

    Proj Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import {BrowserRouter} from 'react-router-dom';
+import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
+
 function App() {
 //hero
 //experience
@@ -5,9 +8,23 @@ function App() {
 //projects
 //footer
   return (
-    <>
-      3D web portfolio
-    </>
+    <BrowserRouter>
+      <div className='relative bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience/>
+        <Tech />
+        <Works />
+        <Feedbacks/>
+        <div className='relative'>
+          <Contact/>
+          <StarsCanvas/>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
