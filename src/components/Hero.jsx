aabfae393cf817e64b3db59179f685e01986a60c
Kadi M <kicktheball1@gmail.com>
2024-07-08 18:24:19 +0200

    Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,19 @@ import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
-      hero
+      <div className={`${styles.paddingX} absolute top-20 max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-purple-600 ' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
+        <h1 className={`${styles.heroHeadText} text-white`}>
+          Hi, I'm <span className='text-purple-500'>Kadi</span>
+        </h1>
+        <p className={`${styles.heroSubText} text-white`}>
+          I develop 3D visuals, user <br className='sm:block hidden'/> interfaces and web applications.
+        </p>
+      </div>
+      <ComputersCanvas />
     </section>
   )
 }
