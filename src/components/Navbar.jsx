cb28ae975798bfe95b576f222c68da7067a408c3
Kadi M <kicktheball1@gmail.com>
2024-07-08 19:20:49 +0200

    Scroll pointer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const  Navbar = () => {
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className={`${styles.padding} w-full flex items-center py-5 fixed top-0  bg-primary`}>
+    <nav className={`${styles.padding} w-full flex items-center py-5 fixed top-0  bg-primary z-10`}>
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link to='/' className='flex items-center gap-2' onClick={()=>{setActive(''); window.scrollTo(0,0);}}>
           <img scr={logo} alt={logo} className='w-4 h-4 object-contain' />
