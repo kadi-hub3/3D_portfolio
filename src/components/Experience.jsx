df0ed140e5ed2447196d1a03a91b59338319c9bd
Kadi M <kicktheball1@gmail.com>
2024-07-10 09:43:26 +0200

    Experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
-import {VerticalTimeline} from "react-vertical-timeline-component";
+import {VerticalTimeline, VerticalTimelineElement} from "react-vertical-timeline-component";
 import { motion } from "framer-motion";
-
 import "react-vertical-timeline-component/style.min.css";
-
 import { styles } from "../styles";
 import { experiences } from "../data";
 import { textVariant } from "../utils/motion";
+
+const ExperienceCard = ({ experience }) => {
+  return (
+    <VerticalTimelineElement
+    
+    >
+      <div>
+        <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+        <p
+          className='text-secondary text-[16px] font-semibold'
+          style={{ margin: 0 }}
+        >
+          {experience.company_name}
+        </p>
+      </div>
+
+      <ul className='mt-5 list-disc ml-5'>
+        {experience.points.map((point, index) => (
+          <li
+            key={`experience-point-${index}`}
+            className='text-white-100 text-[14px] pl-1 tracking-wider'
+          >
+            {point}
+          </li>
+        ))}
+      </ul>
+    </VerticalTimelineElement>
+  );
+};
+
 const Experience = () => {
   return (
     <>
@@ -22,7 +50,10 @@ const Experience = () => {
       <div className='mt-20 flex flex-col'>
         <VerticalTimeline>
           {experiences.map((experience, index) => (
-            <div></div>
+              <ExperienceCard
+                key={`experience-${index}`}
+                experience={experience}
+              />
           ))}
         </VerticalTimeline>
       </div>
