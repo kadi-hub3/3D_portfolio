05df3c26cd40a1950a140413fdde4009d34067a1
Kadi M <kicktheball1@gmail.com>
2024-07-10 09:27:08 +0200

    Experience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import {VerticalTimeline} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
 
+import "react-vertical-timeline-component/style.min.css";
+
+import { styles } from "../styles";
+import { experiences } from "../data";
+import { textVariant } from "../utils/motion";
 const Experience = () => {
   return (
-    <div>Experience</div>
-  )
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={`${styles.sectionSubText} text-center`}>
+          What I have done so far
+        </p>
+        <h2 className={`${styles.sectionHeadText} text-center`}>
+          Work Experience.
+        </h2>
+      </motion.div>
+
+      <div className='mt-20 flex flex-col'>
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <div></div>
+          ))}
+        </VerticalTimeline>
+      </div>
+    </>  )
 }
 
 export default Experience
