ce539586b25a9a2b4d028ca050358189e6e40f12
Kadi M <kicktheball1@gmail.com>
2024-07-11 12:51:22 +0200

    Tech component

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { technologies } from "../data";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          Ball
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Tech
