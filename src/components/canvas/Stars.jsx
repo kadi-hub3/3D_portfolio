394119217e4b5831506d48aa177d8a28066b6f9c
Kadi M <kicktheball1@gmail.com>
2024-07-16 14:31:35 +0200

    star canvas

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,5 +1,16 @@
-import React from 'react'
+import { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
 
+const Stars = (props) => {
+
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+ 
+    </group>
+  );
+};
 const StarsCanvas = () => {
   return (
     <div className='w-full h-auto absolute inset-0 z-[-1]'>
