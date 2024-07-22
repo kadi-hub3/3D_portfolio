02dcc1b254cb69088ec3e5e4e6bf774d9aed1e56
Kadi M <kicktheball1@gmail.com>
2024-07-22 22:30:14 +0200

    Section wrapper

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const SectionWrapper = () => {
-  return (
-    <div>SectionWrapper</div>
-  )
+const SectionWrapper = (Component, idName) => {
+    function HOC () {
+        return (
+            <motion.section
+            variants={staggerContainer()}
+            initial='hidden'
+            whileInView='show'
+            viewport={{ once: true, amount: 0.25 }}
+            className={`max-w-7xl mx-auto relative z-0`}
+          >
+            <span className='hash-span' id={idName}>
+              &nbsp;
+            </span>
+    
+            <Component />
+          </motion.section>          
+          )
+    }
+
 }
 
 export default SectionWrapper
