307daa83cc158b6b649cdb75ae3215fbf656c38a
Kadi M <kicktheball1@gmail.com>
2024-07-22 22:31:21 +0200

    Section wrapper styles

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { styles } from '../styles'
+import { staggerContainer } from '../utils/motion'
+
 
 const SectionWrapper = (Component, idName) => {
     function HOC () {
@@ -8,7 +11,7 @@ const SectionWrapper = (Component, idName) => {
             initial='hidden'
             whileInView='show'
             viewport={{ once: true, amount: 0.25 }}
-            className={`max-w-7xl mx-auto relative z-0`}
+            className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
           >
             <span className='hash-span' id={idName}>
               &nbsp;
